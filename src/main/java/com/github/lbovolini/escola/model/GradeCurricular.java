7551973bb507f137d7f7ee1ac94f2a134a5913c5
Lucas <lbovolini94@gmail.com>
2020-07-07 11:20:58 -0300

    Adicionado modelo GradeCurricular

---
+++ src/main/java/com/github/lbovolini/escola/model/GradeCurricular.java
@@ -0,0 +1,31 @@
+package com.github.lbovolini.escola.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class GradeCurricular {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
