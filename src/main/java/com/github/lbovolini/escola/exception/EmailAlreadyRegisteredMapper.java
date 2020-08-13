e35a8d4f29cef440512e35063a041cc17b3f5253
Lucas <lbovolini94@gmail.com>
2020-08-13 10:51:45 -0300

    Corrigido mensagem de erro

--- src/main/java/com/github/lbovolini/escola/exception/EmailAlreadyRegisteredMapper.java
+++ src/main/java/com/github/lbovolini/escola/exception/EmailAlreadyRegisteredMapper.java
@@ -1,16 +1,21 @@
 package com.github.lbovolini.escola.exception;
 
-import com.github.lbovolini.escola.message.ErrorMessage;
+import com.github.lbovolini.escola.message.FormErrorMessage;
+import com.github.lbovolini.escola.message.InputError;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.ArrayList;
+import java.util.List;
 
 @Provider
 public class EmailAlreadyRegisteredMapper implements ExceptionMapper<EmailAlreadyRegisteredException> {
     @Override
     public Response toResponse(EmailAlreadyRegisteredException e) {
-        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
-        return Response.status(Response.Status.CONFLICT).entity(errorMessage).build();
+        List<InputError> inputErrorList = new ArrayList<>();
+        inputErrorList.add(new InputError("email", e.getMessage()));
+        FormErrorMessage formErrorMessage = new FormErrorMessage(inputErrorList);
+        return Response.status(Response.Status.CONFLICT).entity(formErrorMessage).build();
     }
 }
